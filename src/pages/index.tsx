ab9abf8fc3495cbd409fc281917d64e38ce8d0fe
Rodrigo Carvalho <rhrc09@gmail.com>
2021-04-21 13:09:01 -0300

    feat: add last episodes to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { api } from "../services/api";
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+import styles from "./home.module.scss";
+import Image from "next/image";
 
 type Episode = {
   id: string;
@@ -22,7 +24,36 @@ type HomeProps = {
 };
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
-  return <>{JSON.stringify(latestEpisodes)}</>;
+  return (
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+        <ul>
+          {latestEpisodes.map((episode) => (
+            <li key={episode.id}>
+              <Image
+                width={192}
+                height={192}
+                src={episode.thumbnail}
+                alt={episode.title}
+                objectFit="cover"
+              />
+              <div className={styles.episodeDetails}>
+                <a href="">{episode.title}</a>
+                <p>{episode.members}</p>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+              </div>
+              <button type="button">
+                <img src="/play-green.svg" alt="Tocar episódio" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}></section>
+    </div>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
