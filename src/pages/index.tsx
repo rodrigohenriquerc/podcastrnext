58ef64b5ff90cccf8ec8c45510fcc135a9fb344f
Rodrigo Carvalho <rhrc09@gmail.com>
2021-04-21 11:42:05 -0300

    refactor: update episode object type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import { GetStaticProps } from "next";
 import { api } from "../services/api";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
 type Episode = {
   id: string;
   title: string;
+  thumbnail: string;
+  description: string;
   members: string;
-  published_at: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
 };
 
 type HomeProps = {
@@ -25,9 +33,23 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   });
 
+  const episodes = data.map((episode) => ({
+    id: episode.id,
+    title: episode.title,
+    thumbnail: episode.thumbnail,
+    members: episode.members,
+    publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+      locale: ptBR,
+    }),
+    duration: episode.file.duration,
+    durationAsString: convertDurationToTimeString(episode.file.duration),
+    description: episode.description,
+    url: episode.file.url,
+  }));
+
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   };
